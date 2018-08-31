b48fb2fc0faed2969ca953da7b148f769f7136fa
Ammar Faizi <ammarfaizi2@gmail.com>
2018-08-31 16:39:24 +0700

    Update some files

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -2,4 +2,30 @@
 
 if (! defined("INIT")) {
 	define("INIT", 1);
+
+	require __DIR__."/../../config/init.php";
+	require __DIR__."/../../vendor/autoload.php";
+
+	if (! defined("BASEPATH")) {
+		print "BASEPATH is not defined!\n";
+		exit(1);
+	}
+
+	/**
+	 * @param string $class
+	 * @return void
+	 */
+	function iceTeaInternalAutoloader(string $class): void
+	{
+		$class = str_replace("\\", "/", $class);
+		if (substr($class, 0, 3) === "Phx") {
+			require BASEPATH."/src/phx/".substr($class, 4).".phx";
+		} else {
+			require BASEPATH."/src/classes/".$class.".php";
+		}
+	}
+
+	spl_autoload_register("iceTeaInternalAutoloader");
+
+	require BASEPATH."/src/helpers.php";
 }
