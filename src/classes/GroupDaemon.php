c0762b224a529d4bf8f86e0820abd03f6ceed1e2
Ammar Faizi <ammarfaizi2@gmail.com>
2018-09-01 15:49:08 +0700

    Update GroupDaemon.php

---
+++ src/classes/GroupDaemon.php
@@ -0,0 +1,14 @@
+<?php
+
+use Http\Client;
+use Exceptions\QueueException;
+use Exceptions\HttpClientException;
+use Phx\Polymerization\PyBridge;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com>
+ * @since 0.0.1
+ */
+final class GroupDaemon
+{
+}
